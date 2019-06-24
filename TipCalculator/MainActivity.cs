296b1708765e7d394b544e6025c5b28396395990
Roy Joseph Rimachi Costillas <royexrc@gmail.com>
2019-06-24 18:13:38 -0500

    Add behavior to MainActivity

--- TipCalculator/MainActivity.cs
+++ TipCalculator/MainActivity.cs
@@ -12,6 +12,10 @@ namespace TipCalculator
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        EditText inputBill;
+        Button calculateButton;
+        TextView outputTip;
+        TextView outputTotal;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -21,6 +25,28 @@ namespace TipCalculator
 
             Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
+
+            inputBill = FindViewById<EditText>(Resource.Id.inputBill);
+
+            outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
+
+            calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+            calculateButton.Click += OnCalculateClick;
+        }
+
+        private void OnCalculateClick(object sender, EventArgs e)
+        {
+            string text = inputBill.Text;
+            double bill = 0;
+            if (double.TryParse(text, out bill))
+            {
+                var tip = bill * 0.15;
+                var total = bill + tip;
+
+                outputTip.Text = tip.ToString();
+                outputTotal.Text = total.ToString();
+            }
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
